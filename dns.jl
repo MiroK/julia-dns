89232bdc62dacdf9b2e2d7e300bc1b4cc403e2c1
miro3 <miroslav.kuchta@gmail.com>
2016-05-26 20:06:08 +0200

    bp1

--- dns.jl
+++ dns.jl
@@ -16,8 +16,16 @@ typealias CmplT Complex64
 typealias RArray Array{RealT}
 typealias CArray Array{CmplT}
 
-
-
+# Real vectors
+U = Array[RArray(N, N, Np) for i in 1:3]
+curl = Array[RArray(N, N, Np) for i in 1:3]
+# Complex vectors
+U_hat = Array[RArray(Nh, Np, N) for i in 1:3]
+U_hat0 = Array[RArray(Nh, Np, N) for i in 1:3]
+U_hat1 = Array[RArray(Nh, Np, N) for i in 1:3]
+dU = Array[RArray(Nh, Np, N) for i in 1:3]
+# Complex scalars
+Uc_hat = CArray(Nh, Np, N)
 P_hat = CArray(Nh, Np, N)
 # Transpose
 Uc_hatT = CArray(Nh, N, Np)
@@ -28,16 +36,13 @@ U_mpi = [CArray(Nh, Np, Np) for i in 1:num_processes]
 # Real grid
 x = collect(0:N-1)*2*pi/N
 X = Array[ndgrid(x, x, collect(rank*Np:(rank+1)*Np-1)*2*pi-N)...]
-
-
-# 
-# 
-# kx = fftfreq(N, 1./N)
-# kz = kx[1:(N÷2+1)]; kz[end] *= -1
-# K = ndgrid(kx, kx[(rank*Np+1):((rank+1)*Np-1)], kz)
+# Complex grid
+kx = fftfreq(N, 1./N)
+kz = kx[1:(N÷2+1)]; kz[end] *= -1
+K = Array[ndgrid(kz, kx[(rank*Np+1):((rank+1)*Np-1)], kx)...]
 # K2 = sum(K*K, 0, dtype=int)
 # K_over_K2 = K.astype(float) / where(K2 == 0, 1, K2).astype(float)
-# kmax_dealias = 2./3.*(N/2+1)
+kmax_dealias = 2*Nh/3
 # dealias = array((abs(K[0]) < kmax_dealias)*(abs(K[1]) < kmax_dealias)*
 #                 (abs(K[2]) < kmax_dealias), dtype=bool)
 a = [1./6., 1./3., 1./3., 1./6.]  # Same!
