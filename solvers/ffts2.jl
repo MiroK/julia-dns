b8dc4cfbd674521cb316e019c5a744ed9d238ed1
miro3 <miroslav.kuchta@gmail.com>
2016-06-03 10:37:00 +0200

    ffts2 with mpialltoall

--- solvers/ffts2.jl
+++ solvers/ffts2.jl
@@ -1,4 +1,4 @@
-#import MPI
+using MPI
 
 # Spectral transformation of three dimensional data aligned
 # such that the last component is parallelized across processes
@@ -11,8 +11,10 @@ immutable SpecTransf{T<:Real}
     vT_view::Array{Complex{T}, 4}
     v::Array{Complex{T}, 3}
     v_view::Array{Complex{T}, 4}
+    v_recv::Array{Complex{T}, 3}
+    v_recv_view::Array{Complex{T}, 4}
     # Communicator
-    # comm::MPI.Comm
+    comm::MPI.Comm
     # Amount of data to be send by MPI
     chunk::Int
 
@@ -22,13 +24,15 @@ immutable SpecTransf{T<:Real}
         sizes = size(A)
         N = first(sizes)
         Nh = N÷2+1
-        p = 1  # ;MPI.Comm_size(comm)
+        p = MPI.Comm_size(comm)
         Np = N÷p
         @assert size(A) == (N, N, Np)
 
         # Allocate work arrays
         vT, v = Array{Complex{T}}(Nh, N, Np), Array{Complex{T}}(Nh, Np, N)
         vT_view, v_view = reshape(vT, (Nh, Np, p, Np)), reshape(v, (Nh, Np, Np, p))
+        # For MPI.Alltoall! preallocate the receiving buffer
+        v_recv = similar(v); v_recv_view = reshape(v_recv, (Nh, Np, Np, p))
 
         # Plan Fourier transformations
         plan12 = plan_rfft(A, (1, 2))
@@ -38,7 +42,9 @@ immutable SpecTransf{T<:Real}
 
         chunk = Nh*Np*Np
         # Now we are ready
-        new(plan12, plan3, vT, vT_view, v, v_view, chunk)
+        new(plan12, plan3,
+            vT, vT_view, v, v_view, v_recv, v_recv_view,
+            comm, chunk)
     end
 end
 
@@ -49,17 +55,15 @@ SpecTransf{T<:Real}(A::AbstractArray{T, 3}, comm::Any) = SpecTransf{T}(A, comm)
 function apply{T<:Real}(fu::AbstractArray{Complex{T}, 3}, F::SpecTransf{T}, u::AbstractArray{T})
     A_mul_B!(F.vT, F.plan12, u)
     permutedims!(F.v_view, F.vT_view, [1, 2, 4, 3])
-    #F.v_view[:] = MPI.Alltoall(F.v_view, F.chunk, F.comm)
-    F.plan3*F.v
-    fu[:] = F.v
+    MPI.Alltoall!(F.v_recv_view, F.v_view, F.chunk, F.comm)
+    F.plan3*F.v_recv; fu[:] = F.v_recv
 end
 
 # Transform complex to real as real = T o complex
 function apply_inv{T<:Real}(u::AbstractArray{T}, F::SpecTransf{T}, fu::AbstractArray{Complex{T}, 3})
-    F.plan3.pinv*fu
-    F.v[:] = fu
-    #F.v_view[:] = MPI.Alltoall(F.v_view, F.chunk, F.comm)
-    permutedims!(F.vT_view, F.v_view, [1, 2, 4, 3])
+    F.plan3.pinv*fu; F.v[:] = fu
+    MPI.Alltoall!(F.v_recv_view, F.v_view, F.chunk, F.comm)
+    permutedims!(F.vT_view, F.v_recv_view, [1, 2, 4, 3])
     A_mul_B!(u, F.plan12.pinv, F.vT)
 end
 
@@ -67,10 +71,9 @@ end
 
 
 function foo()
-    #const comm = MPI.COMM_WORLD
-    #const rank = MPI.Comm_rank(comm)
-    ran = 0
-    const num_processes = 1 #MPI.Comm_size(comm)
+    const comm = MPI.COMM_WORLD
+    const rank = MPI.Comm_rank(comm)
+    const num_processes = MPI.Comm_size(comm)
     const N = 64    
     const Nh = N÷2+1
     const Np = N÷num_processes
@@ -80,26 +83,18 @@ function foo()
     fU = Array{Complex{Float64}}(Nh, Np, N)
     ffU = similar(U)
 
-    comm = 1
     const F = SpecTransf(U, comm)
 
-    # Free functions
-    real2spec(U, fU) = apply(fU, F, U)
-    spec2real(fU, U) = apply_inv(U, F, fU)
-
-    for i in 1:30
-        real2spec(U, fU)
-        spec2real(fU, ffU)
-        # apply(fU, F, U)
-        # apply_inv(ffU, F, fU)
+    for i in 1:60
+        apply(fU, F, U)
+        apply_inv(ffU, F, fU)
     end
 
-    # error = MPI.Reduce(sumabs2(ffU-U), MPI.SUM, 0, comm) 
-    error = sumabs2(ffU-U)
-    #rank == 0 && println("Error $(error)")
+    error = MPI.Reduce(sumabs2(ffU-U), MPI.SUM, 0, comm) 
+    error
 end
 
-#MPI.Init()
-#foo()
-#@time foo()
-#MPI.Finalize()
+MPI.Init()
+println(foo())
+@time foo()
+MPI.Finalize()
